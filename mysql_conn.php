3dbd000dc672f5850150c535b453d3bb65762f55
kamaljeet singh <kamaljeet singh>
2015-11-19 15:00:16 +0530

    Dekhle :P

--- mysql_conn.php
+++ mysql_conn.php
@@ -10,7 +10,7 @@
       die('Could not connect: ' . mysql_error());
    }
    
-   echo 'Connected successfully';
+   
    //mysql_close($conn);
 ?>
 
--- real_vote.php
+++ real_vote.php
@@ -37,39 +37,100 @@
 		 }
 ?>
 <h2 style="background-color:Black;color:White;font-size:30;font-family:times new roman;text-align:right;"><b>Cast Your Vote!</b></h2>
+<p style="background-color:Yellow;font-size:30px;"><b>Click On The Cast Vote Of Your Desired Party To Cast Your Vote-</b></p>
 <body background="blue.jpg">
-<form action="register.php" method="post">
-<table border="0" cellspacing ="80">
-                  <?php
+<form action="cast_vote.php" method="post">
+<table border="3" cellspacing ="20" cellpadding="10" >
+                  <tr>
+				  <td width="400"><h1 style="text-align:center;">Party Name</h1></td>
+				  <td width="400"><h1 style="text-align:center;">Candidate Name</h1></td>
+				  <td width="400"><h1 style="text-align:center;">Party Symbol</h1></td>
+				  </tr>
+                     <tr>
+                        <td width="500"><h2 style="text-align:center;">Congress</h2></td>
+						<td width="500"><?php
 				  include 'mysql_conn.php';
 				  
 				  mysql_select_db('kamal_test');
-				  $sql="Select con_no from voter_id,voter_list where e_no=Id";
+				  $sql="Select con_no from voter_id where '$id'=e_no";
 				  $retvalue = mysql_query( $sql, $conn );
 				  $row = mysql_fetch_assoc($retvalue);
 				  $i=101;
-				  echo $row['con_no'];
-				  for($j=0;$i<=103;$i++,$j++)
-				  {		
-					  	
-					  $qry="Select name from voter_id where e_no in (Select e_no from candidate where p_no=$i and con_no=$row['con_no'])"; 
+				  
+				  $qry="Select UPPER(name) from voter_id where e_no in (Select e_no from candidate where p_no='$i' and con_no=".$row['con_no'].")"; 
 					  $retval=mysql_query( $qry, $conn );
 					  $rowww = mysql_fetch_assoc($retval);
-					  $candidate[j]="$rowww['name']";
-				  }
-				   echo $candidate[0]." -- "$candidate[1]." -- "$candidate[2];
-				  ?>
+					  $candidate[0]=$rowww['UPPER(name)'];
 				  
-                     <tr>
-                        <td width="500"><h3>Congress</h3></td>
-						 <td width="500"><h3>Bhartiya Janta Party</h3></td>
-						 <td width="500"><h3>Aam Aadmi Party</h3></td>
-                        
-                     </tr>
-                   <br>
-				   <br>
+				   echo '<span style="padding-left:165px;font-family:Georgia;font-size:30px;color:RED;">'.$candidate[0].'</span>';
+				  ?>
+				  </td>
+				  <td>
+				  
+				<img src="congress.jpg" style="width:150px;height:100px;border:0;padding-left:80px;">
+				<input name="castcong" type="submit" value="Cast Vote">
 					
-</table>                     
+				  </td>
+				  </tr>
+				  
+				  
+				  
+						<tr>
+                        <td width="500"><h2 style="text-align:center;">Bhartiya Janta Party</h2></td>
+						<td width="500"><?php
+				  include 'mysql_conn.php';
+				  
+				  mysql_select_db('kamal_test');
+				  $sql="Select con_no from voter_id where '$id'=e_no";
+				  $retvalue = mysql_query( $sql, $conn );
+				  $row = mysql_fetch_assoc($retvalue);
+				  $i=102;
+				  $qry="Select UPPER(name) from voter_id where e_no in (Select e_no from candidate where p_no='$i' and con_no=".$row['con_no'].")"; 
+					  $retval=mysql_query( $qry, $conn );
+					  $rowww = mysql_fetch_assoc($retval);
+					  $candidate[1]=$rowww['UPPER(name)'];
+				  
+				   echo '<span style="padding-left:165px;font-family:Georgia;font-size:30px;color:RED;">'.$candidate[1].'</span>';
+				  ?>
+				  </td>
+				  <td>
+					
+					<img src="bjp.jpg" style="width:150px;height:100px;border:0;padding-left:80px;">
+					<input name="castbjp" type="submit" value="Cast Vote">
+					
+				  </td>
+				  </tr>
+				  
+						<tr>
+                        <td width="500"><h2 style="text-align:center;">Aam Aadmi Party</h2></td>
+						<td width="500"><?php
+				  include 'mysql_conn.php';
+				  
+				  mysql_select_db('kamal_test');
+				  $sql="Select con_no from voter_id where '$id'=e_no";
+				  $retvalue = mysql_query( $sql, $conn );
+				  $row = mysql_fetch_assoc($retvalue);
+				  $i=103;
+				  
+				  $qry="Select UPPER(name) from voter_id where e_no in (Select e_no from candidate where p_no='$i' and con_no=".$row['con_no'].")"; 
+					  $retval=mysql_query( $qry, $conn );
+					  $rowww = mysql_fetch_assoc($retval);
+					  $candidate[2]=$rowww['UPPER(name)'];
+				  
+				   echo '<span style="padding-left:165px;font-family:Georgia;font-size:30px;color:RED;">'.$candidate[2].'</span>';
+				  ?>
+				  </td>
+				  <td>
+					
+					<img src="aap.jpg" style="width:150px;height:100px;border:0;padding-left:80px;">
+					<input name="castaap" type="submit" value="Cast Vote">
+					
+				  </td>
+				  </tr>
+                  
+					
+</table>
+</form>                     
 </body>
 </html>   
 		 
